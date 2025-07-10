0b6c185ab8e4e00c13c6b79fd16df942df03a2c4
Richard Cook <blubparadox@gmail.com>
2025-07-09 23:23:04 -0400

    Added needed libraries.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule HeadlineMaker.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: [main_module: HeadlineMaker]
     ]
   end
 
@@ -21,8 +22,11 @@ defmodule HeadlineMaker.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:httpoison, "~> 1.8.2"},
+      {:quinn, "~> 1.1.3"},
+      {:readability2, git: "https://github.com/rrcook/readability2"},
+      {:prodigy_objects, git: "https://github.com/rrcook/prodigy_objects.git"},
+      {:naplps_writer, git: "https://github.com/rrcook/naplps_writer.git"}
     ]
   end
 end
