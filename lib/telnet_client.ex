8fac1ee150e5ea7bb7873cd7c20fd2a8124c06fa
Richard Cook <blubparadox@gmail.com>
2025-08-25 21:43:29 -0400

    Simple telnet client we need for retrocampus.

---
+++ lib/telnet_client.ex
@@ -0,0 +1,43 @@
+defmodule TelnetClient do
+  @moduledoc """
+  Simple Telnet client using :gen_tcp.
+  """
+
+  @default_port 23
+
+  @spec open(String.t(), integer()) :: {:ok, port()} | {:error, term()}
+  def open(host, port \\ @default_port) do
+    :gen_tcp.connect(
+      String.to_charlist(host),
+      port,
+      [:binary, active: false, packet: :raw]
+    )
+  end
+
+  @spec close(port()) :: :ok
+  def close(socket) do
+    :gen_tcp.close(socket)
+  end
+
+  @spec send(port(), String.t()) :: :ok | {:error, term()}
+  def send(socket, data) do
+    :gen_tcp.send(socket, data)
+  end
+
+  @spec receive(port(), integer()) :: {:ok, binary()} | {:error, term()}
+  def receive(socket, timeout \\ 5000) do
+    recv(socket, [], timeout)
+  end
+
+  def recv(socket, bs, timeout) do
+    case :gen_tcp.recv(socket, 0, timeout) do
+      {:ok, b} ->
+          recv(socket, [bs, b], timeout);
+      {:error, :closed} ->
+          {:ok, :erlang.list_to_binary(bs)}
+      {:error, :timeout} ->
+          {:ok, :erlang.list_to_binary(bs)}
+    end
+  end
+
+end
