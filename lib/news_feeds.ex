8048a0a814db35f880185237c167202255eff117
Richard Cook <blubparadox@gmail.com>
2025-09-03 22:18:12 -0400

    Extracted out the memorandum style feed into its own module, making a behaviour and leaving behind common code.

--- lib/news_feeds.ex
+++ lib/news_feeds.ex
@@ -16,71 +16,14 @@ defmodule NewsFeeds do
 
   require Logger
 
-  @number_of_feeds 10
   # @margin 60
   @margin 45
   @newline "\r\n "
-  @utf_replace_map %{
-    <<0xE2, 0x80, 0x98>> => "\'",
-    <<0xE2, 0x80, 0x99>> => "\'",
-    <<0xE2, 0x80, 0x9C>> => "\"",
-    <<0xE2, 0x80, 0x9D>> => "\"",
-    <<0xE2, 0x80, 0xA6>> => "..."
-  }
 
-  # Given a news feed XML in the memorandum family, send back an array
-  # of tuples, each item is {headline, story}.
-  # If we can't get the XML pulled down then return an empty array.
-  def get_stories(news_feed, number_of_pages) do
-    try do
-      # The only part that I think will fail, the rest is just string manipulation
+  @callback get_stories(options :: map(), number_of_pages :: non_neg_integer()) :: list(String.t())
 
-      req_body = HTTPoison.get!(news_feed).body
-      IO.inspect("Got the body of #{news_feed}")
-
-      number_of_catches = min(@number_of_feeds, number_of_pages)
-
-      # For our purposes we're just going to chop up the descriptions from the feed
-      # 10 should be enough to get 4 good ones
-      feed = Quinn.parse(req_body)
-      IO.inspect("parse successful")
-
-      article_summaries =
-        Quinn.find(feed, [:item, :description])
-        |> Enum.take(10)
-        |> Enum.map(fn x -> x.value end)
-        |> Enum.map(&hd/1)
-
-      # Take every string HTML and get the readable text out of it.
-      # MOST of the time it's an author, then \n, then a headline and
-      # next text separated by an emdash.
-      # Sometimes there's not an emdash to split on so we filter out
-      # lists that only have one element, then take 4 to pass back.
-      Enum.map(article_summaries, fn s ->
-        s
-        |> Readability.article()
-        |> Readability.readable_text()
-        |> String.replace(Map.keys(@utf_replace_map), fn pat -> @utf_replace_map[pat] end)
-        |> String.split("\n", parts: 2)
-        |> Enum.at(1)
-        # that's an emdash
-        |> String.split("—", parts: 2)
-        |> Enum.map(&String.trim/1)
-      end)
-      |> Enum.filter(fn l -> length(l) == 2 end)
-      |> Enum.take(number_of_catches)
-      |> Enum.map(fn article -> break_on_margin(article, @margin) end)
-    rescue
-      # If we can't get the request just return an empty list
-      e ->
-        IO.inspect("ruh roh")
-        IO.inspect(e)
-        Logger.error(Exception.format(:error, e, __STACKTRACE__))
-        []
-    end
-  end
-
-  defp break_on_margin([hl, body], margin) do
+  @spec break_on_margin([binary(), ...], any()) :: [binary(), ...]
+  def break_on_margin([hl, body], margin \\ @margin) do
     hl_lines = break_text_on_margin(hl, margin)
     body_lines = break_text_on_margin(body, margin)
     [hl_lines, @newline <> body_lines]
