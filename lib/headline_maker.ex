f19d8aac0405857f25e54d0f9b7ff69a8b80014a
Richard Cook <blubparadox@gmail.com>
2025-09-03 22:12:57 -0400

    Add options for retrocampus headline gatherer.

--- lib/headline_maker.ex
+++ lib/headline_maker.ex
@@ -1,25 +1,31 @@
 defmodule HeadlineMaker do
-# Copyright 2025, Ralph Richard Cook
-#
-# This file is part of Prodigy Reloaded.
-#
-# Prodigy Reloaded is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
-# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
-# option) any later version.
-#
-# Prodigy Reloaded is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
-# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License along with Prodigy Reloaded. If not,
-# see <https://www.gnu.org/licenses/>.
-
+  # Copyright 2025, Ralph Richard Cook
+  #
+  # This file is part of Prodigy Reloaded.
+  #
+  # Prodigy Reloaded is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+  # Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+  # option) any later version.
+  #
+  # Prodigy Reloaded is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+  # the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  # GNU Affero General Public License for more details.
+  #
+  # You should have received a copy of the GNU Affero General Public License along with Prodigy Reloaded. If not,
+  # see <https://www.gnu.org/licenses/>.
 
   def main(argv) do
     {opts, _args, _invalid} =
       OptionParser.parse(argv,
-        switches: [input: :string, output: :string, directory: :string, help: :boolean],
-        aliases: [i: :input, o: :output, d: :directory, h: :help]
+        switches: [
+          input: :string,
+          output: :string,
+          directory: :string,
+          help: :boolean,
+          feedstyle: :string,
+          retroguide: :string
+        ],
+        aliases: [i: :input, o: :output, d: :directory, h: :help, f: :feedstyle, r: :retroguide]
       )
 
     cond do
@@ -27,18 +33,33 @@ defmodule HeadlineMaker do
         print_help()
 
       # opts[:input] && opts[:output] ->
-      opts[:input] ->
-        input = opts[:input]
+      true ->
+        input = opts[:input] || "https://memeorandum.com/feed.xml"
         output = opts[:output] || "NH00A000.BDY"
         directory = opts[:directory] || "."
+        retroguide = opts[:retroguide] || "511-1234"
 
-        IO.puts("Input file: #{input}, Output files: #{output}, Directory: #{directory}")
+        feedstyle =
+          case opts[:feedstyle] do
+            nil -> :"Elixir.MemeorandumFeed"
+            style -> String.to_atom("Elixir." <> style)
+          end
 
-        HeadlineWriter.write_headlines(input, output, directory)
+        options = %{
+          input: input,
+          output: output,
+          directory: directory,
+          feedstyle: feedstyle,
+          retroguide: retroguide
+        }
 
-      true ->
-        IO.puts("Invalid options. Use --help for usage.")
-    end
+        IO.puts(
+          "Input file: #{input}, Output files: #{output}, Directory: #{directory}, Feed Style: #{feedstyle}"
+        )
+
+        HeadlineWriter.write_headlines(options)
+
+   end
   end
 
   defp print_help do
